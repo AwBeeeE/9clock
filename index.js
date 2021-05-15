74bbc3ea79dc1c116b1b8690e3c24d6f64606148
LAPTOP-FB86IHHD\eagle <louis_562@hotmail.com>
2021-05-15 12:50:20 +0900

    2nd

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const fs=require("fs");
+const file = fs.readFileSync(".helloworld.txt",{encoding:'utf-8'});
+const stringify = require("csv-stringify/lib/sync")
+const table = [
+    ["name", "job"],
+    ["chicken", "program dog"]
+]
+fs.writeFileSync("Chicken.csv",stringif(table));
+console.log(file);
 for (let i = 0; i < 10; i++)
     console.log("Hello World")
